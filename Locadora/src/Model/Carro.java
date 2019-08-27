9adbad3e3ee15e72a4b1b113555d82aea3ae81a5
Aluno <aluno@etec.sp.gov.br>
2019-08-27 11:34:46 -0300

    Locadora

---
+++ Locadora/src/Model/Carro.java
@@ -0,0 +1,59 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Model;
+
+/**
+ *
+ * @author aluno
+ */
+public class Carro {
+       int id;
+    String modelo, cor, marca, placa;
+    
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getModelo() {
+        return modelo;
+    }
+
+    public void setModelo(String modelo) {
+        this.modelo = modelo;
+    }
+
+    public String getCor() {
+        return cor;
+    }
+
+    public void setCor(String cor) {
+        this.cor = cor;
+    }
+
+    public String getMarca() {
+        return marca;
+    }
+
+    public void setMarca(String marca) {
+        this.marca = marca;
+    }
+
+    public String getPlaca() {
+        return placa;
+    }
+
+    public void setPlaca(String placa) {
+        this.placa = placa;
+    }    
+}
+
+    
+
---
+++ Locadora/src/Model/Clientes.java
@@ -0,0 +1,69 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Model;
+
+/**
+ *
+ * @author aluno
+ */
+public class Clientes {
+       int id;
+    String rg,cpf,estado,cidade,cep;
+    
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getrg() {
+        return rg;
+    }
+
+    public void setModelo(String rg) {
+        this.rg = rg;
+    }
+
+    public String getCpf() {
+        return cpf;
+    }
+
+    public void setCpf(String cor) {
+        this.cpf = cpf;
+    }
+
+    public String getcidade() {
+        return cidade;
+    }
+
+    public void setcidade(String cidade) {
+        this.cidade= cidade;
+    }
+
+    public String getestado() {
+        return estado;
+    }
+
+    public void setestado(String estado) {
+        this.estado = estado;
+    }    
+      public String getcep() {
+        return cep;
+    }
+
+    public void setcep(String cep) {
+        this.cep = cep;
+    }    
+
+    public void setVisible(boolean b) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+}
+
+
